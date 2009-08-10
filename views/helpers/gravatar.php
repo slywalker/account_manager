589b90b1cef6ad270648e752dd65ef10e3a84b08
Yasuo Harada <slywalker.net@gmail.com>
2009-08-10 21:09:33 +0900

    modified gravatar url

--- views/helpers/gravatar.php
+++ views/helpers/gravatar.php
@@ -2,7 +2,7 @@
 class GravatarHelper extends AppHelper {
 
 	public function url($email, $size = 20) {
-		$grav_url = 'http://www.gravatar.com/avatar.php?gravatar_id='.md5(strtolower($email)).'&size='.$size;
+		$grav_url = 'http://www.gravatar.com/avatar/'.md5(strtolower($email)).'?s='.$size;
 		return $grav_url;
 	}
 }
