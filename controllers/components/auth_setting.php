fb145daf15b7866c5b9e77067267c1fd2a206aa0
Yasuo Harada <slywalker.net@gmail.com>
2009-10-06 02:15:44 +0900

    $controller->Auth => $this->Auth

--- controllers/components/auth_setting.php
+++ controllers/components/auth_setting.php
@@ -18,6 +18,7 @@
  **/
 class AuthSettingComponent extends Object {
 
+	public $components = array('Auth', 'Security');
 	/**
 	 * initialize
 	 *
@@ -41,16 +42,12 @@ class AuthSettingComponent extends Object {
 	 * @author Yasuo Harada
 	 */
 	private function __authSettings(&$controller) {
-		if (isset($controller->Auth)) {
-			$controller->Auth->userModel = 'User';
-			$controller->Auth->fields = array('username' => 'email', 'password' => 'hash_password');
-			$controller->Auth->userScope = array('User.disabled' => 0);
-			$controller->Auth->loginAction = array(Configure::read('Routing.admin') => false, 'plugin' => 'account_manager', 'controller' => 'users', 'action' => 'login');
-			$controller->Auth->loginError = __d('account_manager', 'Login failed. Invalid email or username, password.', true);
-			$controller->Auth->authError = __d('account_manager', 'You are not authorized to access that location.', true);
-			//$controller->Auth->allow('*');
-			//$controller->Auth->deny('add', 'edit', 'delete');
-		}
+		$this->Auth->userModel = 'User';
+		$this->Auth->fields = array('username' => 'email', 'password' => 'hash_password');
+		$this->Auth->userScope = array('User.disabled' => 0);
+		$this->Auth->loginAction = array(Configure::read('Routing.admin') => false, 'plugin' => 'account_manager', 'controller' => 'users', 'action' => 'login');
+		$this->Auth->loginError = __d('account_manager', 'Login failed. Invalid email or username, password.', true);
+		$this->Auth->authError = __d('account_manager', 'You are not authorized to access that location.', true);
 	}
 
 	/**
@@ -63,15 +60,11 @@ class AuthSettingComponent extends Object {
 	private function __adminSettings(&$controller) {
 		if (config('basic')) {
 			$users = BASIC_CONFIG::$default;
-			if (isset($controller->Security)) {
-				$controller->Security->loginOptions = array('type'=>'basic');
-				$controller->Security->loginUsers = $users;
-				$controller->Security->requireLogin('*');
-			}
+			$this->Security->loginOptions = array('type' => 'basic');
+			$this->Security->loginUsers = $users;
+			$this->Security->requireLogin('*');
 		}
-		if (isset($controller->Auth)) {
-			$controller->Auth->allow('*');
-		}
+		$this->Auth->allow('*');
 	}
 
 	/**
@@ -82,9 +75,7 @@ class AuthSettingComponent extends Object {
 	 * @author Yasuo Harada
 	 */
 	public function startup(&$controller) {
-		if (isset($controller->Auth)) {
-			Configure::write('AccountManager', $controller->Auth->user());
-		}
+		Configure::write('AccountManager', $this->Auth->user());
 	}
 }
 ?>
